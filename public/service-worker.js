c524fb84e71508f398a43804f88f201c645de37d
Chidi <59777043+Grandmaster@users.noreply.github.com>
2020-04-26 05:05:39 -0400

    Service worker now caches files in the public folder, except for the icons

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,1 +1,19 @@
 // File that will allow the user to use the site even when offline.
+// ===============================================================
+
+// Files to cache
+const files_to_cache = ["/", "/index.html", "/index.js", "/styles.css"];
+
+const cache_name = "static-cache";
+
+// Installing the service worker
+self.addEventListener("install", function (evt) {
+  evt.waitUntil(
+    caches.open(cache_name).then((cache) => {
+      console.log("Your files were pre-cached successfully!");
+      return cache.addAll(files_to_cache);
+    })
+  );
+
+  self.skipWaiting();
+});
